1ed5cf26deb07683cab75dfec787d71caf9b6c78
hermo <hermon.yehdego@miu.edu>
2023-07-15 16:49:50 -0500

    add rest controler

--- git-demo/src/main/java/gitdemo/gitdemo/GitDemoApplication.java
+++ git-demo/src/main/java/gitdemo/gitdemo/GitDemoApplication.java
@@ -3,9 +3,16 @@ package gitdemo.gitdemo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GitDemoApplication implements CommandLineRunner {
+	@GetMapping("/welcome")
+	public  String welcome(){
+		return "welcome to hermon app";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoApplication.class, args);
