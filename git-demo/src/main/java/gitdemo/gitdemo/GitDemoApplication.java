70e8f067b672a36469174b0c0d82d68db01e77a3
hermo <hermon.yehdego@miu.edu>
2023-07-15 15:48:16 -0500

    first commit here

---
+++ git-demo/src/main/java/gitdemo/gitdemo/GitDemoApplication.java
@@ -0,0 +1,18 @@
+package gitdemo.gitdemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("hello hermon");
+	}
+}
